1d798a0574cbd6de317cd84e6a88d1dec0a7fa22
karthikrouthu9 <routhukarthik93@gmail.com>
2015-11-19 06:21:05 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -44,54 +44,9 @@
 }		
 		
 		
-		
-		$.ajaxSetup({
-        xhrFields: {
-            withCredentials: true
-        }
-    });
-	alert("Before Ajax");
-		$.ajax({
-          url:"http://183.82.96.212:8080/services/session/token",
-          type:"get",
-          dataType:"text",
-          crossDomain: true,
-          timeout: 20000,
-          error:function (jqXHR, textStatus, errorThrown) {
-        	  if(textStatus==="timeout") {
-        		  bootbox.dialog({
-        			  closeButton: false,
-        			  message: "Problem connecting with server. Please try after sometime...",
-        			  title: "Alert",
-        			  buttons: {
-        			    success: {
-        			      label: "OK",
-        			      className: "btn-danger",
-        			      callback: function() {
-        			    	  
-        			    	  exit_app();
-        			      }
-        			    
-        			    }
-        			    
-        			    
-        			  }
-        			});
-		        } 
-          },
-          success: function (token) {   
-		alert("Success Token");
-   	//var device_uuid = device.uuid;
-	//var d = document.getElementById("device_uuid");
-	var token =token;
-	var header = "X-CSRF-TOKEN";
-    $(document).ajaxSend(function(e, xhr, options) {
-        xhr.setRequestHeader(header, token);
-    });
-	
 	
 		
-    		is_device_registered();
+    		//is_device_registered();
    alert("Before Custom Function");
  function is_device_registered()
 {
@@ -176,9 +131,6 @@
   return false;
 
 }
-}
-			});
-
     
     var globalVariable;			
      function success(uuid)		
